2d779d67ab0d74044a485bd626ac2b92dc2e77aa
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2023-11-06 14:58:12 +0530

    project cards

--- components/light-box.jsx
+++ components/light-box.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState, useRef, FormEvent } from "react";
 import emailjs from "emailjs-com";
 import { Button } from "@/components/ui/button";
@@ -16,6 +16,8 @@ import { Textarea } from "./ui/textarea";
 import { cn } from "@/lib/utils";
 import LoadingDots from "./loading-dots";
 import logo from "../assets/8.png";
+import { Checkbox } from "./ui/checkbox";
+import Link from "next/link";
 
 export function LightBox({ className }) {
   const [name, setName] = useState("");
@@ -109,12 +111,15 @@ export function LightBox({ className }) {
   };
 
   return (
-    <div className={cn("space-x-4 pt-0 sm:pt-10 ", className)} aria-label="contact form">
+    <div
+      className={cn("space-x-4 pt-0 sm:pt-10 ", className)}
+      aria-label="contact form"
+    >
       <Dialog>
         <DialogTrigger asChild>
           <Button
             variant="default"
-            className="border-2 border-green-300 font-heading border-black bg-gradient-to-b from-green-500 to-blue-600 hover:to-green-500 hover:from-blue-600  w-full"
+            className="border-2 border-green-300 dmsans border-black bg-gradient-to-b from-green-500 to-blue-600 hover:to-green-500 hover:from-blue-600  w-full"
             size="lg"
             aria-label="contact us"
           >
@@ -123,14 +128,19 @@ export function LightBox({ className }) {
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle className="text-center display-animation font-heading font-semibold text-3xl sub-gradient dmsans
-">
+            <DialogTitle
+              className="text-center display-animation dmsans font-semibold text-3xl sub-gradient dmsans
+"
+            >
               Book a Free Design Session
             </DialogTitle>
           </DialogHeader>
           <form ref={form} onSubmit={handleSubmit}>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4" aria-label="enter your name">
+            <div className="grid gap-4 py-4 flex flex-col">
+              <div
+                className="grid grid-cols-1 md:grid-cols-1 items-center gap-4"
+                aria-label="enter your name"
+              >
                 <Label htmlFor="name" className="sub-gradient  font-semibold">
                   Name<span className="error-gradient "> *</span>
                 </Label>
@@ -148,7 +158,10 @@ export function LightBox({ className }) {
                   </div>
                 )}
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4" aria-label="enter your email">
+              <div
+                className="grid grid-cols-1 md:grid-cols-1 items-center gap-4"
+                aria-label="enter your email"
+              >
                 <Label htmlFor="email" className="sub-gradient  font-semibold">
                   Email<span className="error-gradient "> *</span>
                 </Label>
@@ -166,7 +179,10 @@ export function LightBox({ className }) {
                   </div>
                 )}
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4" aria-label="enter your phone number">
+              <div
+                className="grid grid-cols-1 md:grid-cols-1 items-center gap-4"
+                aria-label="enter your phone number"
+              >
                 <Label htmlFor="phone" className="sub-gradient  font-semibold">
                   Phone<span className="error-gradient "> *</span>
                 </Label>
@@ -184,8 +200,14 @@ export function LightBox({ className }) {
                   </div>
                 )}
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4" aria-label="enter your message optional">
-                <Label htmlFor="message" className="sub-gradient pb-2 font-semibold">
+              <div
+                className="grid grid-cols-1 md:grid-cols-1 items-center gap-4"
+                aria-label="enter your message optional"
+              >
+                <Label
+                  htmlFor="message"
+                  className="sub-gradient pb-2 font-semibold"
+                >
                   Message
                 </Label>
                 <Textarea
@@ -196,6 +218,32 @@ export function LightBox({ className }) {
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
                 />
+              </div>
+              <div
+                className="flex flex-row items-center gap-4"
+                aria-label="please check this"
+              >
+                <Checkbox />
+                <div
+                  htmlFor="message"
+                  className="sub-gradient pb-2 dmsans"
+                >
+                  By submitting this you agree to our{" "}
+                  <Link
+                    href="/terms"
+                    className="hover:text-brand  dmsans-bold underline underline-offset-4"
+                  >
+                    Terms of Use
+                  </Link>{" "}
+                  and{" "}
+                  <Link
+                    href="/privacy"
+                    className="hover:text-brand  dmsans-bold underline underline-offset-4"
+                  >
+                    Privacy Policy
+                  </Link>
+                  .
+                </div>
               </div>
             </div>
             {showDialog && (
@@ -211,7 +259,12 @@ export function LightBox({ className }) {
               </Dialog>
             )}
             <DialogFooter>
-              <Button type="submit" className="w-40 mt-2  " disabled={loading} aria-label="Submit contact form">
+              <Button
+                type="submit"
+                className="w-40 mt-2  "
+                disabled={loading}
+                aria-label="Submit contact form"
+              >
                 {loading ? <LoadingDots color="red" /> : "Submit"}
               </Button>
             </DialogFooter>
