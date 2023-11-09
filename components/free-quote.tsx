22b5545205753829cbfe49be4e7d534b31c202a9
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2023-11-09 16:24:01 +0530

    mm

--- components/free-quote.tsx
+++ components/free-quote.tsx
@@ -4,10 +4,10 @@ import { LightBox } from "./light-box";
 function FreeQuote() {
   return (
     <div className="mb-10 mt-10 p-6">
-      <div className="relative overflow-hidden rounded-lg border border-r-blue-400 border-t-green-400 border-s-orange-400 border-b-purple-400 bg-background p-2 mx-auto grid justify-center  gap-4 sm:grid-cols-2 md:max-w-[75rem] md:grid-cols-1">
+      <div className="relative overflow-hidden hover:bg-slate-950 rounded-lg border  border-zinc-400 bg-gray-950 p-2 mx-auto grid justify-center  gap-4 sm:grid-cols-2 md:max-w-[75rem] md:grid-cols-1">
         <div className="flex  flex-col justify-between rounded-md p-6">
           <div className="  mt-1 text-center mx-auto d-block">
-            <p className="text-4xl dmsans-bold font-semibold text-gradient" aria-label="Still have questions">
+            <p className="text-4xl dmsans " aria-label="Still have questions">
               Get a free quote
             </p>
             <LightBox className=" md:mt-5 mt-5 w-60 justify-center mx-auto" />
