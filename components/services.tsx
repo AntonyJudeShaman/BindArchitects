42779e9d1b9874d0d5c05bf39abe339840b3ef55
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2023-11-16 15:21:44 +0530

    ll

--- components/services.tsx
+++ components/services.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Services() {
   return (
-    <div className="md:-ml-32 -ml-4 md:-mr-20 flex p-4 mt-10 flex-col md:flex-col   ">
+    <div className="md:-ml-32 -ml-4 md:-mr-20 flex p-6 mt-10 flex-col md:flex-col   ">
       <div className="flex max-w-[88rem] flex-col text-left">
         <h2 className="dmsans-semibold md:ml-7 ml-8 text-4xl text-left dmsans font-medium leading-[1.1] mb-10 md:text-6xl">
           Our Services<hr className="mt-10 "/>
