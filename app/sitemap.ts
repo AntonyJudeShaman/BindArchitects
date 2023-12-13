e497b48525f164d6b885ea2dbb16f31d0ce2c98f
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2023-12-14 00:11:59 +0530

    faq

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,42 +3,42 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.bindarchitects.com",
+      url: "https://bindarchitects.com",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: "https://bindarchitects.com/",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'daily',
       priority: 1,
     },
     {
         url: "https://bindarchitects.com/contact",
         lastModified: new Date(),
-        changeFrequency: 'weekly',
+        changeFrequency: 'daily',
         priority: 1,
       },
       {
         url: "https://bindarchitects.com/about",
         lastModified: new Date(),
-        changeFrequency: 'weekly',
+        changeFrequency: 'daily',
       },
       {
-        url: "https://bindarchitects.com/frequently-asked-questions",
+        url: "https://bindarchitects.com/faq",
         lastModified: new Date(),
-        changeFrequency: 'weekly',
+        changeFrequency: 'daily',
       },
       {
         url: "https://bindarchitects.com/works",
-        changeFrequency: 'weekly',
+        changeFrequency: 'daily',
         lastModified: new Date(),
       },
       {
         url: "https://bindarchitects.com/projects",
         lastModified: new Date(),
-        changeFrequency: 'weekly',
+        changeFrequency: 'daily',
       },
   ];
 }
