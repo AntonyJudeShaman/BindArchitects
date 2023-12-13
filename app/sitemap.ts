d17e3a0725e397c43353f285de232d803d6c2e21
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2023-12-14 00:23:49 +0530

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,40 +5,156 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://bindarchitects.com",
       lastModified: new Date(),
-      changeFrequency: 'daily',
+      changeFrequency: "daily",
       priority: 1,
     },
     {
       url: "https://bindarchitects.com/",
       lastModified: new Date(),
-      changeFrequency: 'daily',
+      changeFrequency: "daily",
       priority: 1,
     },
     {
-        url: "https://bindarchitects.com/contact",
-        lastModified: new Date(),
-        changeFrequency: 'daily',
-        priority: 1,
-      },
-      {
-        url: "https://bindarchitects.com/about",
-        lastModified: new Date(),
-        changeFrequency: 'daily',
-      },
-      {
-        url: "https://bindarchitects.com/faq",
-        lastModified: new Date(),
-        changeFrequency: 'daily',
-      },
-      {
-        url: "https://bindarchitects.com/works",
-        changeFrequency: 'daily',
-        lastModified: new Date(),
-      },
-      {
-        url: "https://bindarchitects.com/projects",
-        lastModified: new Date(),
-        changeFrequency: 'daily',
-      },
+      url: "https://bindarchitects.com/contact",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://bindarchitects.com/about",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/faq",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/works",
+      changeFrequency: "daily",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://bindarchitects.com/projects",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+
+    {
+      url: "https://bindarchitects.com/projects/delphiderma",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+
+    {
+      url: "https://bindarchitects.com/projects/ccbm",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+
+    {
+      url: "https://bindarchitects.com/projects/arrahmancafe",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/mgasristi",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/eastwest",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/remycinemas",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+
+    {
+      url: "https://bindarchitects.com/projects/duplexhome",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+
+    {
+      url: "https://bindarchitects.com/projects/mokshabar",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/visore",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/kazhinaducafe",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/adst",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/bindoffice",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/gsmt",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/chiettinadyhome",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+
+    {
+      url: "https://bindarchitects.com/projects/casabudget",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/ttdc",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/mancave",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/conferencehome",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/iyerdelights",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/fancyjewellery",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/stationery",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
+    {
+      url: "https://bindarchitects.com/projects/sportszone",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+    },
   ];
 }
